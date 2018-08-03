2b6baa845fc54cf20a48e55ee9ef73f48fc1ee6b
haleykeizur <haleykz@yahoo.com>
2018-08-03 19:41:24 +0000

    Done.

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,3 +1,7 @@
-class Quiz
+def stress_relief_quiz
+  stress_relief = ["Go to a concert with your friends", "Get lunch at your favorite dining spot", "Go bowling", "Wake up early to go on a hike", "Watch the sunset at a park with your friends", "Try a food or restaurant you have never tried", "Go shopping and treat yourself to something", "Have a spa day by yourself", "Get icecream on a sunny day", "Have a Netflix marathon", "Have a game night with friends or family", "Have a movie night with your favorite junk food", "Go to your local gym or trampoline park", "Learn to play a new instrument", "Light a candle and start a new book"]
 
+return stress_relief.sample
 end
+
+puts stress_relief_quiz
--- config.ru
+++ config.ru
@@ -1,3 +1,5 @@
+require 'sinatra'
 require './config/environment'
+require_relative './app/controllers/application_controller.rb'
 
 run ApplicationController
