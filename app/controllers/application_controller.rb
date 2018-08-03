c64dd74226c9cd914f9c0268623f389fe864969b
haleykeizur <haleykz@yahoo.com>
2018-08-03 17:18:23 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
-class ApplicationController < Sinatra::Base
+require 'sinatra'
 
+class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
+  
+  get '/' do
+    erb :index
+  end
+ 
+ post '/' do
+    @stress_relief_activity = stress_relief_quiz
+    @the_user = params[:user]
+    return erb :results
+    return erb :index
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
-class Question
+# puts "Please enter your name"
+# name = gets.strip 
+# stress_relief = stress_relief.sample
 
-end
+# "Hey #{name} We know all of this studying is hard work, but it will all pay off! Whenever you feel overwhelmed with all your hard work, take a break. For example, you can: #{stress_relief} Treat yourself. You’ve earned it!"
